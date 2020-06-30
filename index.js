1eca3754b4bd0640ad37b9ece597081db4023372
Leonardo Bonetti <leonardo.bonetti.silva@gmail.com>
2020-06-30 10:32:15 -0300

    fds

---
+++ index.js
@@ -0,0 +1,23 @@
+(function () {
+
+    var video = document.getElementById('video');
+
+    navigator.getMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+    navigator.getMedia({
+        video: true,
+        audio: false
+    }, function (stream) {
+        video.srcObject = stream;
+        video.play();
+
+    },
+        function (error) {
+
+        }
+    )
+
+})();
